85e2e6212b0ce04c2752642c079e87e14eb64eb4
Efe Genç <efegenc44@hotmail.com>
2023-06-22 16:42:23 +0300

    BigInt operations

--- src/bigint.rs
+++ src/bigint.rs
@@ -8,6 +8,14 @@ impl BigInt {
     pub fn is_positive(&self) -> bool {
         self.sign == Sign::Positive
     }
+
+    pub fn abs(&self) -> Self {
+        let mut result = self.clone();
+        if self.is_negative() {
+            result.sign = Sign::Positive;
+        }
+        result
+    }
 }
 
 impl APNum for BigInt {
@@ -32,6 +40,156 @@ impl APNum for BigInt {
 
     fn digit_count(&self) -> usize {
         self.natural.digits.len()
+    }
+}
+
+impl std::ops::Add for &BigInt {
+    type Output = BigInt;
+
+    fn add(self, rhs: Self) -> Self::Output {
+        use Sign::*;
+
+        match (&self.sign, &rhs.sign) {
+            (Positive, Positive) => BigInt {
+                sign: Sign::Positive,
+                natural: &self.natural + &rhs.natural,
+            },
+            (Negative, Negative) => BigInt {
+                sign: Sign::Negative,
+                natural: &self.natural + &rhs.natural,
+            },
+            (Positive, Negative) => &self.natural - &rhs.natural,
+            (Negative, Positive) => &rhs.natural - &self.natural,
+            (_, Zero) => self.clone(),
+            (Zero, _) => rhs.clone(),
+        }
+    }
+}
+
+impl std::ops::Add for BigInt {
+    type Output = BigInt;
+
+    fn add(self, rhs: Self) -> Self::Output {
+        (&self).add(&rhs)
+    }
+}
+
+impl std::ops::Mul for &BigInt {
+    type Output = BigInt;
+
+    fn mul(self, rhs: Self) -> Self::Output {
+        use Sign::*;
+
+        let sign = match (&self.sign, &rhs.sign) {
+            (Positive, Positive) | (Negative, Negative) => Positive,
+            (Positive, Negative) | (Negative, Positive) => Negative,
+            (_, Zero) | (Zero, _) => Zero,
+        };
+
+        BigInt {
+            sign,
+            natural: &self.natural * &rhs.natural,
+        }
+    }
+}
+
+impl std::ops::Mul for BigInt {
+    type Output = BigInt;
+
+    fn mul(self, rhs: Self) -> Self::Output {
+        (&self).mul(&rhs)
+    }
+}
+
+impl std::ops::Sub for &BigInt {
+    type Output = BigInt;
+
+    fn sub(self, rhs: Self) -> Self::Output {
+        self + &-rhs
+    }
+}
+
+impl std::ops::Sub for BigInt {
+    type Output = BigInt;
+
+    fn sub(self, rhs: Self) -> Self::Output {
+        (&self).sub(&rhs)
+    }
+}
+
+impl std::ops::Div for &BigInt {
+    type Output = (BigInt, BigInt);
+
+    fn div(self, rhs: Self) -> Self::Output {
+        use Sign::*;
+
+        let (q, r) = &self.natural / &rhs.natural;
+        let (q, r): (BigInt, BigInt) = (q.into(), r.into());
+
+        match (&self.sign, &rhs.sign) {
+            (Positive, Positive) => (q, r),
+            (Negative, Negative) => (q, -r),
+            (Positive, Negative) => (-q - BigInt::from(1), rhs + &r),
+            (Negative, Positive) => (-q - BigInt::from(1), rhs - &r),
+            (Zero, _) => (BigInt::zero(), BigInt::zero()),
+            // Handled at BigNat division
+            (_, Zero) => unreachable!(),
+        }
+    }
+}
+
+impl std::ops::Div for BigInt {
+    type Output = (BigInt, BigInt);
+
+    fn div(self, rhs: Self) -> Self::Output {
+        (&self).div(&rhs)
+    }
+}
+
+impl std::ops::Neg for &BigInt {
+    type Output = BigInt;
+
+    fn neg(self) -> Self::Output {
+        let mut result = self.clone();
+        result.sign = match self.sign {
+            Sign::Positive => Sign::Negative,
+            Sign::Negative => Sign::Positive,
+            Sign::Zero => Sign::Zero,
+        };
+        result
+    }
+}
+
+impl std::ops::Neg for BigInt {
+    type Output = BigInt;
+
+    fn neg(self) -> Self::Output {
+        -(&self)
+    }
+}
+
+impl std::cmp::Ord for BigInt {
+    fn cmp(&self, other: &Self) -> std::cmp::Ordering {
+        use std::cmp::Ordering::*;
+        use Sign::*;
+
+        match (&self.sign, &other.sign) {
+            (Positive, Positive) => self.natural.cmp(&other.natural),
+            (Negative, Negative) => self.natural.cmp(&other.natural).reverse(),
+            (Positive, Negative) => Greater,
+            (Negative, Positive) => Less,
+            (Positive, Zero) => Greater,
+            (Negative, Zero) => Less,
+            (Zero, Negative) => Greater,
+            (Zero, Positive) => Less,
+            (Zero, Zero) => Equal,
+        }
+    }
+}
+
+impl std::cmp::PartialOrd for BigInt {
+    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
+        Some(self.cmp(other))
     }
 }
 
@@ -67,6 +225,15 @@ macro_rules! impl_from_integer {
 
 impl_from_integer!(usize u8 u16 u32 u64 ;
                    isize i8 i16 i32 i64);
+
+impl From<BigNat> for BigInt {
+    fn from(value: BigNat) -> Self {
+        BigInt {
+            sign: Sign::Positive,
+            natural: value,
+        }
+    }
+}
 
 impl std::str::FromStr for BigInt {
     type Err = APNumParseError;
---
+++ tests/bigint_test.rs
@@ -0,0 +1,97 @@
+use apnum::{APNum, BigInt};
+
+#[test]
+fn bigint_add() {
+    let x = BigInt::zero();
+    let y = BigInt::from(123);
+    assert_eq!(&x + &y, y);
+    let x = BigInt::from(321);
+    let y = BigInt::from(-296);
+    assert_eq!(x + y, BigInt::from(25));
+    let x = BigInt::from(-321);
+    let y = BigInt::from(12);
+    assert_eq!(x + y, BigInt::from(-309));
+    let x = BigInt::from(77);
+    let y = BigInt::from(-33);
+    assert_eq!(x + y, BigInt::from(44));
+}
+
+#[test]
+fn bigint_mul() {
+    let x = BigInt::zero();
+    let y = BigInt::from(123);
+    assert_eq!(&x * &y, BigInt::zero());
+    let x = BigInt::from(321);
+    let y = BigInt::from(-296);
+    assert_eq!(x * y, BigInt::from(-95016));
+    let x = BigInt::from(-321);
+    let y = BigInt::from(12);
+    assert_eq!(x * y, BigInt::from(-3852));
+    let x = BigInt::from(-77);
+    let y = BigInt::from(-33);
+    assert_eq!(x * y, BigInt::from(2541));
+}
+
+#[test]
+fn bigint_sub() {
+    let x = BigInt::from(100);
+    let y = BigInt::from(-98);
+    assert_eq!(&x - &y, BigInt::from(198));
+    assert_eq!(&y - &x, BigInt::from(-198));
+    assert_eq!(&x - &x, BigInt::zero());
+
+    let x = BigInt::from(4464);
+    let y = BigInt::from(-18);
+    assert_eq!(&x - &y, BigInt::from(4482));
+    assert_eq!(&y - &x, BigInt::from(-4482));
+
+    let x = BigInt::from(5);
+    assert_eq!(&x - &BigInt::zero(), BigInt::from(5));
+    assert_eq!(&BigInt::zero() - &x, BigInt::from(-5));
+
+    assert_eq!(BigInt::zero() - BigInt::zero(), BigInt::zero());
+}
+
+#[test]
+fn bigint_div() {
+    let x = BigInt::from(-1000);
+    let y = BigInt::from(900);
+    assert_eq!(&x / &y, (BigInt::from(-2), BigInt::from(800)));
+    let x = BigInt::from(42);
+    let y = BigInt::from(-10);
+    assert_eq!(&x / &y, (BigInt::from(-5), BigInt::from(-8)));
+    let x = BigInt::from(43);
+    let y = BigInt::from(-2);
+    assert_eq!(&x / &y, (BigInt::from(-22), BigInt::from(-1)));
+    let x = BigInt::from(-789);
+    let y = BigInt::from(-34);
+    assert_eq!(&x / &y, (BigInt::from(23), BigInt::from(-7)));
+    assert_eq!(&-x / &y, (BigInt::from(-24), BigInt::from(-27)));
+    let x = BigInt::from(0);
+    let y = BigInt::from(2);
+    assert_eq!(&x / &y, (BigInt::zero(), BigInt::zero()));
+}
+
+#[test]
+fn bigint_cmp() {
+    let x = BigInt::from(123);
+    let y = BigInt::from(321);
+    assert!(x < y);
+    assert!(-x < y);
+    let x = BigInt::from(123);
+    let y = BigInt::from(21);
+    assert!(x > y);
+    assert!(-x < -y);
+    let x = BigInt::from(4);
+    let y = BigInt::from(4);
+    assert!(x == y);
+    assert!(x > -y);
+    let x = BigInt::from(0);
+    let y = BigInt::from(0);
+    assert!(x == y);
+    assert!(x == -y);
+    let x = BigInt::from(0);
+    let y = BigInt::from(4);
+    assert!(x < y);
+    assert!(x > -y);
+}
